869de7e3f6b00ea7141bf0fcaf617712cd205940
AssoDePicche <samuelprado730@gmail.com>
2022-12-30 00:00:02 -0300

    Feat: Add Input and Output

---
+++ code/Introduction/inputAndOutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int n1, n2, n3;
+
+    std::cin >> n1 >> n2 >> n3;
+    
+    std::cout << n1 + n2 + n3;
+
+    return 0;
+}
