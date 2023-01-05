b54b180fa1b09373f0ebf17245fd426133972a0d
AssoDePicche <samuelprado730@gmail.com>
2023-01-05 13:26:10 -0300

    Refactor: Remove unnecessary libraries

--- code/Introduction/inputAndOutput.cpp
+++ code/Introduction/inputAndOutput.cpp
@@ -1,16 +1,12 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 
 int main()
 {
     int n1, n2, n3;
 
-    std::cin >> n1 >> n2 >> n3;
-    
-    std::cout << n1 + n2 + n3;
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    printf("%d", n1 + n2 + n3);
 
     return 0;
 }
