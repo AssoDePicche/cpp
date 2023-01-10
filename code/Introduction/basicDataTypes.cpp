1c705dbd82a7eec793ce649f999dbf9d4c0ec73d
AssoDePicche <samuelprado730@gmail.com>
2023-01-10 01:28:44 -0300

    Style: Format Basic Data Types

--- code/Introduction/basicDataTypes.cpp
+++ code/Introduction/basicDataTypes.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
-int main()
-{
-    int d;
+int main() {
+  int d;
 
-    long ld;
+  long ld;
 
-    char c;
+  char c;
 
-    float f;
+  float f;
 
-    double lf;
+  double lf;
 
-    scanf("%d %ld %c %f %lf", &d, &ld, &c, &f, &lf);
+  scanf("%d %ld %c %f %lf", &d, &ld, &c, &f, &lf);
 
-    printf("%d\n%ld\n%c\n%f\n%lf", d, ld, c, f, lf);
+  printf("%d\n%ld\n%c\n%f\n%lf", d, ld, c, f, lf);
 
-    return 0;
+  return 0;
 }
