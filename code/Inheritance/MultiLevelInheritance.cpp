cc900a1b6a48970a37a460255097a974c3c4a7c6
AssoDePicche <samuelprado730@gmail.com>
2023-01-10 15:59:30 -0300

    Feat: Add Multi Level Inheritance

---
+++ code/Inheritance/MultiLevelInheritance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Triangle {
+  public:
+    void triangle() {
+        printf("I am a triangle\n");
+    }
+};
+
+class Isosceles : public Triangle {
+  public:
+    void isosceles() {
+        printf("I am an isosceles triangle\n");
+    }
+};
+
+class Equilateral : public Isosceles {
+  public:
+    void equilateral() {
+        printf("I am an equilateral triangle\n");
+    }
+};
