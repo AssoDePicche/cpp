ec61c70889ab7ba4640048f09a6fff0ae87e1820
AssoDePicche <samuelprado730@gmail.com>
2023-04-18 00:28:37 -0300

    refactor: extract student methods implementation

--- src/Classes/Class/Student.cpp
+++ src/Classes/Class/Student.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
+#include "./Student.h"
 
-class Student {
-  private:
-    int age, standard;
+int Student::get_age()
+{
+  return age;
+}
 
-    std::string first_name, last_name;
+void Student::set_age(int _age)
+{
+  age = _age;
+}
 
-  public:
-    int get_age() {
-      return age;
-    }
+std::string Student::get_first_name()
+{
+  return first_name;
+}
 
-    void set_age(int _age) {
-      age = _age;
-    }
+void Student::set_first_name(std::string _first_name)
+{
+  first_name = _first_name;
+}
 
-    std::string get_first_name() {
-      return first_name;
-    }
+std::string Student::get_last_name()
+{
+  return last_name;
+}
 
-    void set_first_name(std::string _first_name) {
-      first_name = _first_name;
-    }
+void Student::set_last_name(std::string _last_name)
+{
+  last_name = _last_name;
+}
 
-    std::string get_last_name() {
-      return last_name;
-    }
+int Student::get_standard()
+{
+  return standard;
+}
 
-    void set_last_name(std::string _last_name) {
-      last_name = _last_name;
-    }
+void Student::set_standard(int _standard)
+{
+  standard = _standard;
+}
 
-    int get_standard() {
-      return standard;
-    }
-
-    void set_standard(int _standard) {
-      standard = _standard;
-    }
-
-    std::string to_string() {
-      return std::to_string(age) + "," + first_name + "," + last_name + "," + std::to_string(standard);
-    }
-};
+std::string Student::to_string()
+{
+  return std::to_string(age) + "," + first_name + "," + last_name + "," + std::to_string(standard);
+}
