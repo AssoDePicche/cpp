3a0b61cec076a4d10570e45de6352f000b49001c
AssoDePicche <samuelprado730@gmail.com>
2023-04-22 17:35:21 -0300

    refactor: apply changes

--- src/Classes/Class/Solution.cpp
+++ src/Classes/Class/Solution.cpp
@@ -9,22 +9,13 @@ int main()
 
   std::cin >> age >> first_name >> last_name >> standard;
 
-  Student student;
-
-  student.set_age(age);
-
-  student.set_standard(standard);
-
-  student.set_first_name(first_name);
-
-  student.set_last_name(last_name);
+  Student student(first_name, last_name, age, standard);
 
   std::cout << student.get_age() << std::endl;
 
-  std::cout << student.get_last_name() << ", " << student.get_first_name() << std::endl;
+  std::cout << student.get_last_name() << std::endl;
 
-  std::cout << student.get_standard() << std::endl
-            << std::endl;
+  std::cout << student.get_standard() << std::endl << std::endl;
 
   std::cout << student.to_string();
 
