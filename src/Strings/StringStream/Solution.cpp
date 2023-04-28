13a0bbebef124f5e6c96a1a9b9aeb25957cb80ca
AssoDePicche <samuelprado730@gmail.com>
2023-04-27 22:00:47 -0300

    refactor: update code design

--- src/Strings/StringStream/Solution.cpp
+++ src/Strings/StringStream/Solution.cpp
@@ -1,34 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include "./StringParser.cpp"
 
-std::vector<int> parseInts(std::string str) {
-  std::stringstream stream(str);
+int main()
+{
+  std::string string;
 
-  std::vector<int> integers;
+  std::cin >> string;
 
-  char comma;
+  std::vector<int> integers = StringParser::toIntegers(string);
 
-  int integer;
-
-  while (stream >> integer) {    
-    integers.push_back(integer);
-
-    stream >> comma;
-  }
-
-  return integers;
-}
-
-int main() {
-  std::string str;
-
-  std::cin >> str;
-
-  std::vector<int> integers = parseInts(str);
-
-  for (int integer : integers) {
-    std::cout << integer << "\n";
+  for (const int integer : integers)
+  {
+    std::cout << integer << std::endl;
   }
 
   return 0;
