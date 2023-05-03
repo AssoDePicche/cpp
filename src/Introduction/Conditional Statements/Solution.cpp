e6627ea34c7cee264302568d976b15dbe8fc7f93
AssoDePicche <samuelprado730@gmail.com>
2023-05-03 13:22:11 -0300

    refactor: improve conditional statements design

--- src/Introduction/Conditional Statements/Solution.cpp
+++ src/Introduction/Conditional Statements/Solution.cpp
@@ -2,20 +2,15 @@
 
 int main()
 {
-  std::string numbers[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  const std::string list[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
   int number;
 
   std::cin >> number;
 
-  if (number > 9)
-  {
-    std::cout << "Greater than 9" << std::endl;
-  }
-  else
-  {
-    std::cout << numbers[number - 1] << std::endl;
-  }
+  std::string message = (number > 9) ? "Greater than 9" : list[number - 1];
+
+  std::cout << message << std::endl;
 
   return 0;
 }
