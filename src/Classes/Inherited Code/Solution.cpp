6906d959d0b06639d99c89fe0393fb644d7fdd80
AssoDePicche <samuelprado730@gmail.com>
2023-05-19 22:42:43 -0300

    refactor: update includes

--- src/Classes/Inherited Code/Solution.cpp
+++ src/Classes/Inherited Code/Solution.cpp
@@ -1,40 +1,18 @@
-#include "./BadLengthException.hpp"
+#include "checkUsername.hpp"
 #include <iostream>
 #include <string>
 
-bool checkUsername(std::string username)
-{
-  bool is_valid = true;
-
-  int username_length = username.length();
-
-  if (username_length < 5)
-  {
-    throw BadLengthException(username_length);
-  }
-
-  for (int index = 0; index < username_length - 1; index++)
-  {
-    if (username[index] == 'w' && username[index + 1] == 'w')
-    {
-      is_valid = false;
-    }
-  }
-
-  return is_valid;
-}
-
 int main()
 {
-  int inputs;
+  int validations;
 
   bool is_valid;
 
   std::string username, validation_message;
 
-  std::cin >> inputs;
+  std::cin >> validations;
 
-  while (inputs-- != 0)
+  while (validations-- != 0)
   {
     std::cin >> username;
 
